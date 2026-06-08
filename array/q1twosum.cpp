f058d087327f0f72487fc8c86d2dd8978601f293
Neha19087 <neha.aarav2023@gmail.com>
2026-06-08 15:33:26 +0530

    two sum

--- array/q1twosum.cpp
+++ array/q1twosum.cpp
@@ -1,3 +1,6 @@
+//this is brute force method which uses a nested array to iterate the whole array to find if the complement of 
+// selected index exists such that their sum equals to target
+//time complexity is almost O(n^2)
 #include <bits/stdc++.h>
 using namespace std;
  vector<int> twoSum(vector<int>& nums, int target) {
@@ -27,3 +30,46 @@ int main(){
         cout<< it<<" ";
     }
 }
+// better solution for it would be using hash map
+//time complexity is almost O(nlogn)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mpp;
+        int n= nums.size();
+        for(int i=0;i<n;i++){
+            int rem= target- nums[i];
+            if (mpp.find(rem)!=mpp.end()){
+                return {i,mpp[rem]};
+            }
+            mpp[nums[i]]=i;
+        }
+        return {};
+    }
+};
+
+// this is an optimal solution involving two pointer method which can be used only when array is sorted 
+//time complexity is almost O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n= numbers.size();
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            if (numbers[left]+numbers[right]<target){
+                left++;
+            }
+            else if(numbers[left]+numbers[right]>target){
+                right--;
+            }
+            else{
+                return {left, right};
+            }               
+            
+        }
+        return {};
+        
+    }
+};
