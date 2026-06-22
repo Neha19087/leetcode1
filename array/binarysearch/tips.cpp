402d350393afcad86652e243319079574ef09438
Neha19087 <neha.aarav2023@gmail.com>
2026-06-22 21:33:26 +0530

    when to use binary search

---
+++ array/binarysearch/tips.cpp
@@ -0,0 +1,5 @@
+ //when to use binary search
+ //when given array is sorted and min or max of any value is asked
+ // when we have to achieve logarithmic time complexity
+ //when we have to minimise steps
+  
