999e6a81930f38b0ef2d848c4464e8f3a65bd066
Neha19087 <neha.aarav2023@gmail.com>
2026-07-08 22:35:16 +0530

    copy  a  linked list with random pointer

---
+++ linkedlist/q138.cpp
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node*temp=head;
+        while(temp!=NULL){
+            Node *copynode= new Node(temp->val);
+            copynode->next= temp->next;
+            temp->next=copynode;
+            temp=temp->next->next;
+        }
+        temp=head;
+        while(temp!=NULL){
+            Node*copynode=temp->next;
+            if(temp->random!=NULL) {
+                copynode->random= temp->random->next;
+            }
+            else{
+                copynode->random=NULL;
+            }
+            temp=temp->next->next;
+        }
+        temp=head;
+        Node dummy(-1);
+        Node*res= &dummy;
+        while(temp!=NULL){
+            res->next=temp->next;
+            temp->next=temp->next->next;
+            res=res->next;
+            temp=temp->next;
+        }
+        return( dummy.next);
+    }
+};
