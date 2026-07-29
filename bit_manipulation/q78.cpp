e5167a6419559e909d3f056e26eb57425188a26f
Neha19087 <neha.aarav2023@gmail.com>
2026-07-29 21:24:30 +0530

    counting prime no

---
+++ bit_manipulation/q78.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void helper(vector<int>& nums,vector<vector<int>>& ans,int index,vector<int>& ds){
+        if(index==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+        
+        ds.push_back(nums[index]);
+        helper(nums,ans,index+1,ds);
+        ds.pop_back();
+        helper(nums,ans,index+1,ds);
+        
+        return;
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int>ds;
+        helper(nums,ans,0,ds);
+        return ans;
+    }
+};
